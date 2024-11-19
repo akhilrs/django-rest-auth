d8f57c4b4c58a0505802a192c6fd200e470c3efd
Mohamed Shiyas B <mohamed.shiyas@qusol.in>
2024-11-19 09:54:17 +0530

    U-820/task-822: changes in rest_auth/regsitration/serializer.py and rest_auth/utils.py for new python version

--- rest_auth/registration/serializers.py
+++ rest_auth/registration/serializers.py
@@ -4,8 +4,8 @@ from django.contrib.auth import get_user_model
 
 try:
     from allauth.account import app_settings as allauth_settings
-    from allauth.utils import (email_address_exists,
-                               get_username_max_length)
+    from allauth.utils import get_username_max_length
+    from rest_auth.utils import email_address_exists
     from allauth.account.adapter import get_adapter
     from allauth.account.utils import setup_user_email
     from allauth.socialaccount.helpers import complete_social_login
